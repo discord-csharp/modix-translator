9c87b4c540b63bffc9a244c420c6757bcaf653df
CallumMcLoughlin <59436970+CallumMcLoughlin@users.noreply.github.com>
2022-01-11 22:37:59 +1300

    Rename how-to channel to translation-how-to

--- src/ModixTranslator/TranslationConstants.cs
+++ src/ModixTranslator/TranslationConstants.cs
@@ -4,7 +4,7 @@ namespace ModixTranslator
     public class TranslationConstants
     {
         public const string CategoryName = "localized";
-        public const string HowToChannelName = "how-to";
+        public const string HowToChannelName = "translation-how-to";
         public const string HistoryChannelName = "history";
         public static readonly string[] PermanentChannels = new[] { HowToChannelName, HistoryChannelName };
         public const string StandardLanguage = "en";
